96055f385d56d172d201475bdcc34f36ff4cacd7
Tomek Weksej <tomek.weksej@gmail.com>
2024-02-21 16:14:32 +0100

    Logreg work in progress

--- ps1/src/linearclass/logreg.py
+++ ps1/src/linearclass/logreg.py
@@ -10,12 +10,27 @@ def main(train_path, valid_path, save_path):
         valid_path: Path to CSV file containing dataset for validation.
         save_path: Path to save predicted probabilities using np.savetxt().
     """
+    print(f'******* {train_path}')
     x_train, y_train = util.load_dataset(train_path, add_intercept=True)
+    x_valid, y_valid = util.load_dataset(valid_path, add_intercept=True)
+
+    util.plot(x_train, y_train, None, f'{train_path}.png')
+    # util.plot(x_valid, y_valid, None, f'{valid_path}.png')
 
     # *** START CODE HERE ***
     # Train a logistic regression classifier
+    lr = LogisticRegression()
+    # lr.fit(x_train, y_train)
+    x_train_small = x_train[:2, :]
+    y_train_small = y_train[:2]
+    y_train_small[1] = 1.
+    lr.fit(x_train_small, y_train_small)
+
     # Plot decision boundary on top of validation set set
+    # util.plot(x_valid, y_valid, lr.theta, f'{valid_path}.png')
+
     # Use np.savetxt to save predictions on eval set to save_path
+    # np.savetxt(save_path, lr.predict(x_valid))
     # *** END CODE HERE ***
 
 
@@ -51,6 +66,69 @@ class LogisticRegression:
             y: Training example labels. Shape (n_examples,).
         """
         # *** START CODE HERE ***
+        n, dim = x.shape
+        if self.theta is None:
+            self.theta = np.zeros((dim,))
+        
+        print_matrix(x, 'x')
+        print_matrix(y, 'y')
+        
+
+        # while True:
+        for i in range(10000):
+            h = self.predict(x)
+            print_matrix(h, 'h')
+            dl = -1./n * x.T @ (y - h)
+            print_matrix(dl, 'dl')
+            H = 1./n * x.T @ np.diag(h * (1 - h)) @ x
+            print_matrix(H, 'H')
+            delta = np.linalg.inv(H) @ dl
+            print_matrix(delta, 'delta')
+            self.theta = self.theta - delta
+            norm = np.linalg.norm(delta, 1)
+            print(f'epoch {i}: {norm}')
+            if norm < self.eps:
+                return
+        # *** END CODE HERE ***
+
+    def deprecated_fit(self, x, y):
+        """Run Newton's Method to minimize J(theta) for logistic regression.
+
+        Args:
+            x: Training example inputs. Shape (n_examples, dim).
+            y: Training example labels. Shape (n_examples,).
+        """
+        # *** START CODE HERE ***
+        n, dim = x.shape
+        if self.theta is None:
+            self.theta = np.zeros((dim, 1))
+        
+        y = y[:, np.newaxis]
+        
+        print_matrix(x, 'x')
+        print_matrix(y, 'y')
+        
+        x_outer = x[:, :, np.newaxis] * x[:, np.newaxis, :]
+        print_matrix(x_outer, 'x_outer')
+
+        # while True:
+        for i in range(10000):
+            h = self.predict(x)
+            print_matrix(h, 'h')
+            dl = -1./n * np.sum(x * (y - h), axis=0)
+            dl = dl[:, np.newaxis]
+            print_matrix(dl, 'dl')
+            pr = np.expand_dims(h * (1 - h), axis=-1)
+            print_matrix(pr, 'pr')
+            H = 1./n * np.sum(pr * x_outer, axis=0)
+            print_matrix(H, 'H')
+            delta = np.linalg.inv(H) @ dl
+            print_matrix(delta, 'delta')
+            self.theta = self.theta - delta
+            norm = np.linalg.norm(delta, 1)
+            print(f'epoch {i}: {norm}')
+            if norm < self.eps:
+                return
         # *** END CODE HERE ***
 
     def predict(self, x):
@@ -63,7 +141,22 @@ class LogisticRegression:
             Outputs of shape (n_examples,).
         """
         # *** START CODE HERE ***
+        def sigmoid(x):
+            return 1. / (1. + np.exp(-x))
+
+        print_matrix(self.theta, 'theta')
+        print_matrix(x, 'x')
+        preds = sigmoid(x @ self.theta.T)
+        print_matrix(preds, 'preds')
+
+        return preds
         # *** END CODE HERE ***
+
+def print_matrix(m, name):
+    print(f'{name}: {m.shape}')
+    print(m)
+    # pass
+
 
 if __name__ == '__main__':
     main(train_path='ds1_train.csv',
--- ps1/src/linearclass/util.py
+++ ps1/src/linearclass/util.py
@@ -75,11 +75,12 @@ def plot(x, y, theta, save_path, correction=1.0):
     plt.plot(x[y == 1, -2], x[y == 1, -1], 'bx', linewidth=2)
     plt.plot(x[y == 0, -2], x[y == 0, -1], 'go', linewidth=2)
 
-    # Plot decision boundary (found by solving for theta^T x = 0)
-    x1 = np.arange(min(x[:, -2]), max(x[:, -2]), 0.01)
-    x2 = -(theta[0] / theta[2] + theta[1] / theta[2] * x1
-           + np.log((2 - correction) / correction) / theta[2])
-    plt.plot(x1, x2, c='red', linewidth=2)
+    if theta is not None:
+        # Plot decision boundary (found by solving for theta^T x = 0)
+        x1 = np.arange(min(x[:, -2]), max(x[:, -2]), 0.01)
+        x2 = -(theta[0] / theta[2] + theta[1] / theta[2] * x1
+            + np.log((2 - correction) / correction) / theta[2])
+        plt.plot(x1, x2, c='red', linewidth=2)
     plt.xlim(x[:, -2].min()-.1, x[:, -2].max()+.1)
     plt.ylim(x[:, -1].min()-.1, x[:, -1].max()+.1)
 
