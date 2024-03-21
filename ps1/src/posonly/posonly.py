746bf798cd6478cede3e0d978daa5629fb478c12
Tomek Weksej <tomek.weksej@gmail.com>
2024-03-21 12:23:16 +0100

    Solve 2b

--- ps1/src/posonly/posonly.py
+++ ps1/src/posonly/posonly.py
@@ -27,23 +27,26 @@ def main(train_path, valid_path, test_path, save_path):
     output_path_adjusted = save_path.replace(WILDCARD, 'adjusted')
 
     x_train, t_train = util.load_dataset(train_path, label_col='t', add_intercept=True)
+    _, y_train = util.load_dataset(train_path, label_col='y', add_intercept=True)
     x_test, t_test = util.load_dataset(test_path, label_col='t', add_intercept=True)
 
-    xlim, ylim = util.get_lim(x_train, x_test)
+    xlim, ylim = util.get_lim([x_train, x_test], 4)
 
     util.plot(x_train, t_train, None, f'{train_path}.png', xlim, ylim)
 
     # *** START CODE HERE ***
     # Part (a): Train and test on true labels
-    util.print_matrix(x_train, 'x_train')
     lr = LogisticRegression(verbose=False)
     lr.fit(x_train, t_train)
     util.plot(x_test, t_test, lr.theta, f'{output_path_true}.png', xlim, ylim)
     np.savetxt(output_path_true, lr.predict(x_test))
 
-    # Make sure to save predicted probabilities to output_path_true using np.savetxt()
     # Part (b): Train on y-labels and test on true labels
-    # Make sure to save predicted probabilities to output_path_naive using np.savetxt()
+    lr = LogisticRegression(verbose=False)
+    lr.fit(x_train, y_train)
+    util.plot(x_test, t_test, lr.theta, f'{output_path_naive}.png', xlim, ylim)
+    np.savetxt(output_path_naive, lr.predict(x_test))
+
     # Part (f): Apply correction factor using validation set and test on true labels
     # Plot and use np.savetxt to save outputs to output_path_adjusted
     # *** END CODER HERE
